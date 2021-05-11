dca299750152b41f8628c4207f8a94df39b8f6d6
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-11 16:50:26 +0800

    structure done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+puts "Creating articles"
+10.times do
+    art = Article.create(title: Faker::Cannabis.brand,
+    content: Faker::Cannabis.health_benefit)
+    puts "#{art.title} created!"
+end
